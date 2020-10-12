966f0d31976220434f1de1e6bd7a71943a271416
xiangxinji <xxjbbking@163.com>
2020-10-12 22:53:46 +0800

    通知组件基本结束

--- packages/Notification/index.stories.js
+++ packages/Notification/index.stories.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Notic from './index';
+import LeButton from '../Button';
 import '../../src/styles/index.scss';
 
 Vue.use(Notic);
@@ -10,9 +11,7 @@ export default {
 
 export const baseUsage = () => ({
   components: {
-  },
-  mounted() {
-
+    LeButton,
   },
   methods: {
     addNotice() {
@@ -20,28 +19,80 @@ export const baseUsage = () => ({
     },
   },
   template: `
-        <button @click="addNotice"> add notice </button>
+        <LeButton @click="addNotice"> add notice </LeButton>
     `,
 });
 
 export const autoClose = () => ({
   components: {
-  },
-  mounted() {
-
+    LeButton,
   },
   methods: {
     addNotice() {
       this.$notice({ autoClose: false });
     },
     addNotice1() {
-      this.$notice({ timeout: 1000  });
+      this.$notice({ timeout: 1000 });
     },
   },
   template: `
        <div>
-       <button @click="addNotice" > add notice , autoclose = false </button>
-       <button @click="addNotice1" > add notice , timout = 1000 </button>
+         <LeButton @click="addNotice" > add notice , autoclose = false </LeButton>
+         <LeButton @click="addNotice1" > add notice , timout = 1000 </LeButton>
+       </div>
+    `,
+});
+
+export const title = () => ({
+  components: {
+    LeButton,
+  },
+  methods: {
+    addNotice() {
+      this.$notice({ autoClose: false, title: '系统提示' });
+      this.$notice({ autoClose: false, title: '系统提示', message: '当前环境并不安全' });
+    },
+  },
+  template: `
+       <div>
+       <LeButton @click="addNotice" > add notice , title  </LeButton>
+       </div>
+    `,
+});
+
+export const showClose = () => ({
+  components: {
+    LeButton,
+  },
+  methods: {
+    addNotice() {
+      this.$notice({ autoClose: false, title: '系统提示', message: '时间不在于你拥有多少,而在于你怎样使用啊啊啊啊啊啊啊啊啊啊' });
+    },
+  },
+  template: `
+       <div>
+       <LeButton @click="addNotice" > add notice , title  </LeButton>
+       </div>
+    `,
+});
+
+export const addHtmlTag = () => ({
+  components: {
+    LeButton,
+  },
+  methods: {
+    addNotice() {
+      this.$notice({
+        autoClose: false, title: '系统提示系统提示系统提示系统提示系统提示', message: `
+        你的控制台好像出现了一点问题,
+        <a class="link" href="https://www.baidu.com"> go</a>
+      `,
+      });
+    },
+  },
+  template: `
+       <div>
+       <LeButton @click="addNotice" > add notice , title  </LeButton>
        </div>
     `,
 });
--- packages/Notification/src/index.vue
+++ packages/Notification/src/index.vue
@@ -1,16 +1,26 @@
 <template>
   <div :class="wrapperClass" :style="wrapperStyle">
-    notification {{ top }}
-    <button @click="handleClose"> close</button>
+    <div class="close" @click="handleClose">
+      <le-icon icon="times" color="#ccc" v-if="showClose"></le-icon>
+    </div>
+      <div class="title" v-if="title">
+        {{ title }}
+      </div>
+      <div class="message-panel" v-html="message">
+      </div>
   </div>
 </template>
 
 <script>
+import Icon from '../../Icon';
 import NotificationManager from './notification';
 
 export default {
   name: 'LeNotification',
   componentName: 'le.notification',
+  components: {
+    LeIcon: Icon,
+  },
   props: {
     autoClose: {
       type: Boolean,
@@ -18,7 +28,13 @@ export default {
     },
     timeout: {
       type: Number,
-      default: 3000,
+      default: 8000,
+    },
+    title: String,
+    message: String,
+    showClose: {
+      type: Boolean,
+      default: true,
     },
   },
   data() {
@@ -30,7 +46,6 @@ export default {
     };
   },
   mounted() {
-    console.log(this);
     if (this.autoClose) {
       setTimeout(() => {
         this.handleClose();
@@ -44,6 +59,7 @@ export default {
         top: `${this.top}px`,
         zIndex: this.notificationManager.index,
         opacity: this.opacity,
+        right: '10px',
       };
     },
     wrapperClass() {
